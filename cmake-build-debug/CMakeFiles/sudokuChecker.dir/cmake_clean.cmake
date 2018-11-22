ad8768bd01654d5658d6bea811d053943278a2e5
Caleb Ghormley <calebghormley@yahoo.com>
2018-11-21 22:17:32 -0700

    Multithreading practice

---
+++ cmake-build-debug/CMakeFiles/sudokuChecker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudokuChecker.dir/main.c.obj"
+  "sudokuChecker.pdb"
+  "sudokuChecker.exe"
+  "sudokuChecker.exe.manifest"
+  "libsudokuChecker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sudokuChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
